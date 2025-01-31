6a09a8ce08958632d02127d76a831ce5dd9ebf29
PrinceSP <princepasombaran@gmail.com>
2025-01-31 18:12:39 +0700

    feat: add hero component and about component to main app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { About, Hero } from './components'
+
+function App() {
+
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero/>
+      <About/>
+    </main>
+  )
+}
+
+export default App
