c2fa857baad3bb875bd06ec7c53115e42ff4364f
PrinceSP <princepasombaran@gmail.com>
2025-01-31 18:11:01 +0700

    feat: reate a requsable button

---
+++ src/components/button/index.tsx
@@ -0,0 +1,13 @@
+const Button = ({id,title,leftIcon,containerClass}) => {
+  return (
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black ${containerClass}`}>
+      {leftIcon}
+
+      <span className="relative overflow-hidden uppercase incline-flex font-general text-xs">
+        <p>{title}</p>
+      </span>
+    </button>
+  )
+}
+
+export default Button
