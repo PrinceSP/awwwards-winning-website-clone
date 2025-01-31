0b4a50bebb588500e6e1eb014fedb05b54a69cea
PrinceSP <princepasombaran@gmail.com>
2025-01-31 18:11:48 +0700

    fix: finishing the hero section animation and content

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,135 @@
+import { useEffect, useRef, useState } from "react"
+import { TiLocationArrow } from "react-icons/ti"
+import gsap from 'gsap'
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from 'gsap/all'
+import Button from "../button"
+
+gsap.registerPlugin(ScrollTrigger)
+
+const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loadedVideos === totalVideos - 1) {
+      setIsLoading(false);
+    }
+  }, [loadedVideos]);
+
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
+  };
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
+
+  const getVideoSrc = (index:number) => `videos/hero-${index}.mp4`;
+
+  return (
+    <main className="relative h-dvh w-screen">
+      {
+        isLoading &&
+        <section className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </section>
+      }
+      <section id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+        <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+          <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
+            <video id="current-video" ref={nextVideoRef} src={getVideoSrc((currentIndex % totalVideos) + 1)} loop muted className="origin-center size-64 scale-150 object-cover object-center" onLoadedData={handleVideoLoad} />
+          </div>
+        </div>
+        <video
+          ref={nextVideoRef}
+          id="next-video"
+          loop
+          src={getVideoSrc((currentIndex))}
+          className="absolute-center z-20 invisible size-64 object-cover object-center"
+          onLoadedData={handleVideoLoad}
+        />
+        <video className="absolute left-0 top-0 size-full object-cover object-center" loop autoPlay muted src={getVideoSrc((currentIndex === totalVideos - 1 ? 1 : currentIndex))} onLoadedData={handleVideoLoad} />
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 text-blue-75 z-40">
+          G<b>a</b>ming
+        </h1>
+
+        <section className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">redefi
+              <b>n</b>
+              <b>e</b>
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br />
+              Unleash the Play Economy
+            </p>
+
+            <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="bg-yellow-300 flex-center gap-1" />
+          </div>
+        </section>
+      </section>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>a</b>ming
+      </h1>
+    </main>
+  )
+}
+
+export default Hero
