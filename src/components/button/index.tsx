6c1e1b4255c82dfbcbcdc49d46e6ded756cbe2e3
PrinceSP <princepasombaran@gmail.com>
2025-02-03 12:17:52 +0700

    feat: add icon to the rist side after text

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,11 @@
-const Button = ({id,title,leftIcon,containerClass}) => {
+const Button = ({id,title,leftIcon,rightIcon,containerClass}) => {
   return (
     <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black ${containerClass}`}>
       {leftIcon}
-
       <span className="relative overflow-hidden uppercase incline-flex font-general text-xs">
         <p>{title}</p>
       </span>
+      {rightIcon}
     </button>
   )
 }
