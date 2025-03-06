920f775f89ac83fab8d1e09263f5316273b0c08e
PrinceSP <princepasombaran@gmail.com>
2025-03-06 18:56:29 +0700

    chore: export navbar component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Hero from "./hero";
 import About from "./about";
 import AnimatedText from "./animatedText";
+import Navbar from "./navbar";
 
-export { Hero, About, AnimatedText }
+export { Hero, About, AnimatedText, Navbar }
