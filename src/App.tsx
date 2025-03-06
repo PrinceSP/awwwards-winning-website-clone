ef4f193071fed20a7ed954bd1192a99316a05b69
PrinceSP <princepasombaran@gmail.com>
2025-03-06 18:49:14 +0700

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { About, Hero } from './components'
+import { About, Hero, Navbar } from './components'
 
 function App() {
 
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar/>
       <Hero/>
       <About/>
     </main>
